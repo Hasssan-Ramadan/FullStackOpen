17e14d8fe53bc9c4a7b0ae3b710a6936f8b16479
Hassan Ramadan <RmdanJrx@gmail.com>
2022-02-28 19:38:31 +0200

    4.8: Blog list tests, step1 done

---
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: '5a422aa71b54a676234d17fb',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422ba71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b5ba676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a67623fd17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676e34d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aae1b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234717f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b56a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '493119960a439565123c06e7',
+    title: 'Who am I',
+    author: 'Hassan Ramadan',
+    url: 'https://github.com/rmdanjr/rmdanjr',
+    likes: 43,
+    __v: 0,
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  initialBlogs.forEach(async (blog) => {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  })
+})
+
+describe('Test HTTP GET /api/blogs', () => {
+  test('all blogs are returned', async () => {
+    let response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+
+  test(' returned data is in json format', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,5 +1,10 @@
 require('dotenv').config()
 
-const { PORT, MONGODB_URL } = process.env
+const PORT = process.env.PORT
+
+const MONGODB_URL =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URL
+    : process.env.MONGODB_URL
 
 module.exports = { MONGODB_URL, PORT }
