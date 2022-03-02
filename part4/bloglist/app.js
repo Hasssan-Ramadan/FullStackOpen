9e69101786e47ad447f26ea946b1be64b5d1fcc7
Hassan Ramadan <RmdanJrx@gmail.com>
2022-03-02 19:02:14 +0200

    4.18: bloglist expansion, step6 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -16,5 +17,6 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
