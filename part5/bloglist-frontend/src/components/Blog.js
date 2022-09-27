b312378b6b319856a23d9fa7ab0ec64d288c33c7
Hassan Ramadan <RmdanJrx@gmail.com>
2022-09-27 12:00:24 +0200

    5.7* Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [view, setView] = useState(false);
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleView = () => setView(!view);
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} - {blog.author}
+        <button onClick={toggleView}>{view ? "hide" : "view"}</button>
+      </p>
+      <div style={{ display: view ? "block" : "none" }}>
+        <p>{blog.url}</p>
+        <p>
+          likes {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.user.name}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
