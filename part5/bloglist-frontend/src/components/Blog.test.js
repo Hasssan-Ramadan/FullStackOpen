8bc6907c0be37e1168b5b686308024ca00c84317
Hassan Ramadan <RmdanJrx@gmail.com>
2022-09-28 14:31:25 +0200

    5.14: Blog list tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,12 +1,15 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 
 import Blog from "./Blog";
 
 /*
- * checks that the component displaying a blog
- * renders the blog's title and author,
- * but does not render its url or number of likes by default.
+ * checks that the component displaying a blog:
+ * 1. renders the blog's title and author by default.
+ * 2. does not render its url or number of likes by default.
+ * 3. shows the blog's url and number of likes when the button controlling the shown details has been clicked.
  */
 describe("<Blog/>", () => {
   const user = {
@@ -30,8 +33,8 @@ describe("<Blog/>", () => {
       { exact: false }
     );
     const authorEl = screen.getByText("Ahmed Ali", { exact: false });
-    expect(titleEl.parentElement).toHaveStyle("display: block");;
-    expect(authorEl.parentElement).toHaveStyle("display: block");;
+    expect(titleEl.parentElement).toHaveStyle("display: block");
+    expect(authorEl.parentElement).toHaveStyle("display: block");
   });
 
   test("does not render its url or number of likes by default", () => {
@@ -40,5 +43,17 @@ describe("<Blog/>", () => {
     const likesEl = screen.getByText("23", { exact: false });
     expect(urlEl.parentElement).toHaveStyle("display: none");
     expect(likesEl.parentElement).toHaveStyle("display: none");
+  });
+
+  test("shows the blog's url and number of likes when the button controlling the shown details has been clicked.", async () => {
+    const user = userEvent.setup();
+    render(<Blog blog={blog} user={user} />);
+    const btnEl = screen.getByText("view");
+    await user.click(btnEl);
+    const urlEl = screen.getByText("https://google.com");
+    const likesEl = screen.getByText("23", { exact: false });
+    expect(btnEl.textContent).toBe("hide");
+    expect(urlEl.parentElement).toHaveStyle("display: block");
+    expect(likesEl.parentElement).toHaveStyle("display: block");
   });
 });
